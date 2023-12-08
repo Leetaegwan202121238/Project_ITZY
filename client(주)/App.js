d710a5b7aaf07596015abe613affe66e1c3347fe
JJY0 <92962527+JJY0@users.noreply.github.com>
2023-12-08 22:55:37 +0900

    Update App.js

--- client(주)/App.js
+++ client(주)/App.js
@@ -7,10 +7,6 @@ import Signin from './components/Signin/Signin';
 
 // 다른 컴포넌트들을 여기서 import합니다.
 
-/**
- *
- * 아 그니까 이 파일은 /join을 리디렉션 하면 join.js 컴포넌트를 보여줘라(렌더링?) 하는 거 지정하는 코드 적어놨음
- */
 function App() {
     return (
         <Router>
@@ -26,5 +22,4 @@ function App() {
     );
 }
 
-// App을 수출할 준비가 됐다
 export default App;
