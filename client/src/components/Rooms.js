a1ee615a6a6ec77691d0714c2ea105af5010ecd4
Leetaegwan202121238 <5516q@naver.com>
2023-12-13 01:10:01 +0900

    version 12/13

--- client/src/components/Rooms.js
+++ client/src/components/Rooms.js
@@ -9,7 +9,7 @@ const Rooms = () => {
 
   const fetchRoomsFromServer = async () => {
     try {
-      const response = await fetch('54.87.129.134:5000/rooms');
+      const response = await fetch('http://localhost:5000/rooms');
       const data = await response.text(); // 텍스트로 받음
       setRooms(data.split(',')); // 텍스트를 원하는 형식으로 처리, 예시로 쉼표(,)로 구분된 데이터를 배열로 변환
     } catch (error) {
