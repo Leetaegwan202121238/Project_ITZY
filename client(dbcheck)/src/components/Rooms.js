6e42ce0d00c3611148459b4da85e05a8f1a03eb4
KIM_DEAHO <102588838+DHowor1d@users.noreply.github.com>
2023-12-14 13:05:59 +0900

    Add files via upload

--- client(dbcheck)/src/components/Rooms.js
+++ client(dbcheck)/src/components/Rooms.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Rooms.css'; // 
 
 const Rooms = () => {
   const [rooms, setRooms] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     fetchRoomsFromServer();
@@ -10,21 +13,28 @@ const Rooms = () => {
   const fetchRoomsFromServer = async () => {
     try {
       const response = await fetch('http://localhost:5000/rooms');
-      const data = await response.text(); // 텍스트로 받음
-      setRooms(data.split(',')); // 텍스트를 원하는 형식으로 처리, 예시로 쉼표(,)로 구분된 데이터를 배열로 변환
+      const data = await response.json(); 
+      setRooms(data); // 데이터를 상태로 설정
     } catch (error) {
       console.error('방 목록을 불러오는 중 오류 발생:', error);
     }
   };
 
+  const goBack = () => {
+    navigate(-1);
+  }
+
   return (
-    <div>
-      <h2>사용 가능한 방</h2>
-      <ul>
+    <div className="rooms-container">
+      <button className="title-button">사용 가능한 방</button>
+      <ul className="room-list">
         {rooms.map((room) => (
-          <li key={room}>{room}</li>
+          <li key={room} className="room-item">
+            <button disabled className="room-button">{room}</button>
+          </li>
         ))}
       </ul>
+      <button className="back-button" onClick={goBack}>뒤로 가기</button>
     </div>
   );
 };
